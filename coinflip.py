85f8cff02b9fc575df3d0a89755d5f7c6b935003
mia <143556556+amia39@users.noreply.github.com>
2026-07-27 16:49:44 -0700

    basic coinflip features done

--- coinflip.py
+++ coinflip.py
@@ -9,6 +9,8 @@ bot = discord.Bot()
 
 coinflips = bot.create_group("coins", "flip coins")
 
+diceroll = bot.create_group("dice", "roll dice")
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
@@ -21,9 +23,15 @@ async def flip(ctx: discord.ApplicationContext):
     embed = discord.Embed(title=res)
     await ctx.respond(embed=embed)
 
-@coinflips.command(description="Flip multiple coins")
-async def flipmore(ctx: discord.ApplicationContext):
-    await ctx.respond("")
+@coinflips.command(description="Flip multiple coins (1 or more)")
+async def flipmore(ctx: discord.ApplicationContext, num: int):
+    embed = discord.Embed(title="Invalid number of coins", description="Enter at least 1 (Max: 99999)")
+    if 0 < num < 100000:
+        res = 0
+        for i in range(num):
+            res += random.randint(0, 1)
+        embed = discord.Embed(title=f'Heads: {res}, Tails: {num - res}')
+    await ctx.respond(embed=embed)
 
 # run bot with bot token
 bot.run(os.getenv("TOKEN"))
